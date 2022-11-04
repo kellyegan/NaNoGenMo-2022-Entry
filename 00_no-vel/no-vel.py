550d977ad691e5b37870113fd3c672f3d1899a78
Kelly Egan <kelly.egan@gmail.com>
2022-11-03 22:19:00 -0400

    Add chapters

--- 00_no-vel/no-vel.py
+++ 00_no-vel/no-vel.py
@@ -8,31 +8,66 @@ import markdown
 from weasyprint import HTML, CSS
 
 
-def createNovel():
+def create_no_phrase(min_nos, max_nos):
+    phrase = ""
+    word_count = 0
+    for words in range(random.randrange(min_nos, max_nos)):
+        phrase += "n" + random.choice([oString(random.randint(1, 10)), "o", "o"]) + " "
+        word_count += 1
+    return phrase.strip(), word_count
+
+
+def create_chapter_titles(num_chapters):
+    chapter_titles = []
+    for i in range(0,num_chapters):
+        title, title_length = create_no_phrase(1,10)
+        chapter_titles.append(title.capitalize())
+
+    return chapter_titles
+
+
+def create_novel(novel_word_length=50000):
     novel = "\n\n# No-vel\n\n\n"
-    wordCount = 0
+    word_count = 0
+    chapter_word_count = 0
 
-    while wordCount < 50000:
-        novel += "N" + random.choice([oString(random.randint(1,10)), "o", "o"])
-        for words in range(random.randrange(2,25)):
-            novel += " n" + random.choice([oString(random.randint(1,10)), "o", "o"])
-            wordCount += 1
-        novel += random.choice(['.','.','.','.','.','.','.','.','.','?','?','!'])
+    chapter_titles = create_chapter_titles(10)
+    target_chapter_length = novel_word_length / 10
+    current_chapter_length = random.randrange(min(0, target_chapter_length - 500), max(target_chapter_length + 500, novel_word_length - word_count))
+    print(current_chapter_length, word_count)
 
-        #Add paragraph break
+    while word_count < novel_word_length:
+
+        new_phrase, words_in_sentence = create_no_phrase(2, 25)
+        novel += new_phrase.capitalize()
+        novel += random.choice(['.', '.', '.', '.', '.', '.', '.', '.', '.', '?', '?', '!'])
+
+        word_count += words_in_sentence
+        chapter_word_count += words_in_sentence
+
+        # Add paragraph break
         if random.random() > 0.8:
             novel += "\n\n"
         else:
             novel += " "
 
+        print(current_chapter_length, chapter_word_count)
+
+        if chapter_word_count >= current_chapter_length:
+            chapter_word_count = 0
+            novel += "\n\n## " + chapter_titles.pop() + "\n\n"
+            current_chapter_length = random.randrange(min(0, target_chapter_length - 500),
+                                                      max(target_chapter_length + 500, novel_word_length - word_count))
+
     return novel
 
 
 def oString(x):
-    count = random.randint(1,x)
-    max = random.randint(1,x)
+    count = random.randint(1, x)
+    max = random.randint(1, x)
     count = min(count, max)
     return "o" * count
+
 
 def createPDF(novelString):
     novel_html = markdown.markdown(novelString)
@@ -40,10 +75,9 @@ def createPDF(novelString):
     return htmldoc.write_pdf()
 
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    novel = createNovel()
+    novel = create_novel()
     novel_pdf = createPDF(novel)
     f = open("no-vel.pdf", "wb")
     f.write(novel_pdf)
