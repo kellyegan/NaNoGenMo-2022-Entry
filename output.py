ca80c4280d3473faaf0163fd4fe7c9b577ba7834
Kelly Egan <kelly.egan@gmail.com>
2022-11-06 21:53:05 -0500

    Add some css.

--- output.py
+++ output.py
@@ -1,11 +1,17 @@
 import markdown
 from weasyprint import HTML, CSS
+import os
 
 
 def create_pdf(markdown_string):
     html_string = markdown.markdown(markdown_string)
     html_doc = HTML(string=html_string, base_url="")
-    return html_doc.write_pdf()
+
+    css_file = "style.css"
+    css_string = open(css_file, encoding="UTF-8").read()
+    base_css = CSS(string=css_string)
+
+    return html_doc.write_pdf(stylesheets=[base_css])
 
 
 def save_pdf(pdf, path):
