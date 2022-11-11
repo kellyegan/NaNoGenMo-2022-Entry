eee3d76b80dd08a0ea614f6edc24a65a2bddbeac
Kelly Egan <kelly.egan@gmail.com>
2022-11-11 14:37:09 -0500

    Fix find_words function

--- n02_markov.py
+++ n02_markov.py
@@ -1,6 +1,3 @@
-import os
-
-
 def find_word(query, list_of_strings):
     return [s for s in list_of_strings if query in s]
 
@@ -11,8 +8,20 @@ def find_words(query_list, list_of_strings):
         for query in query_list:
             if query in s:
                 results.append(s)
-            break;
+                break;
+    return results
 
 
 if __name__ == '__main__':
-    print("hello")
+    data_directory = "sentence_data"
+
+    file_path = data_directory + "/" + "60.txt"
+
+    with open(file_path, "r") as f:
+        sentences = f.readlines()[100:110]
+        no_sentences = find_words(["devil", "himself"], sentences)
+        print(sentences)
+        print(no_sentences)
+
+
+
